2b7b37fe615806d9bfcac138c57e5fd810ef3ab7
Raymond's MacBook Pro <raymond16@hotmail.co.nz>
2022-12-03 20:37:27 +1300

    Add title output

---
+++ app.js
@@ -0,0 +1,41 @@
+window.addEventListener('load', function () {
+    function generateTitle() {
+        const app = document.querySelector('#app');
+        const issue = document.querySelector('#issue');
+        const error = document.querySelector('#error');
+        const titleOutput = document.querySelector('#title');
+
+        if (app.value && issue.value) {
+        }
+
+        app.addEventListener('input', function () {
+            titleOutput.value = `${app.value}`;
+        });
+
+        issue.addEventListener('input', function () {
+            if (app.value) {
+                titleOutput.value = `${app.value}`;
+            }
+            if (app.value && issue.value) {
+                titleOutput.value = `${app.value} - ${issue.value}`;
+            }
+        });
+
+        error.addEventListener('input', function () {
+            if (app.value) {
+                titleOutput.value = `${app.value}`;
+            }
+            if (app.value && issue.value) {
+                titleOutput.value = `${app.value} - ${issue.value}`;
+            }
+            if (app.value && issue.value && error.value) {
+                titleOutput.value = `${app.value} - ${issue.value} - ${error.value}`;
+            }
+            if (app.value && issue.value === '' && error.value) {
+                titleOutput.value = `${app.value} - ${error.value}`;
+            }
+        });
+    }
+
+    generateTitle();
+});
