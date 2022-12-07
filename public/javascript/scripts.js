f0fa2809c1855bbe82db59e7a00c0ddad7a9bf0b
Raymond's MacBook Pro <raymond16@hotmail.co.nz>
2022-12-07 19:11:13 +1300

    Add incident text formatter

--- public/javascript/scripts.js
+++ public/javascript/scripts.js
@@ -1,38 +1,34 @@
 window.addEventListener('load', function () {
-    function generateTitle() {
-        const app = document.querySelector('#app');
-        const issue = document.querySelector('#issue');
-        const error = document.querySelector('#error');
-        const titleOutput = document.querySelector('#title');
-
-        app.addEventListener('input', function () {
-            titleOutput.value = `${app.value}`;
-        });
-
-        issue.addEventListener('input', function () {
-            if (app.value) {
-                titleOutput.value = `${app.value}`;
-            }
-            if (app.value && issue.value) {
-                titleOutput.value = `${app.value} - ${issue.value}`;
-            }
-        });
-
-        error.addEventListener('input', function () {
-            if (app.value) {
-                titleOutput.value = `${app.value}`;
-            }
-            if (app.value && issue.value) {
-                titleOutput.value = `${app.value} - ${issue.value}`;
-            }
-            if (app.value && issue.value && error.value) {
-                titleOutput.value = `${app.value} - ${issue.value} - ${error.value}`;
-            }
-            if (app.value && issue.value === '' && error.value) {
-                titleOutput.value = `${app.value} - ${error.value}`;
-            }
-        });
-    }
-
-    generateTitle();
+    // function generateTitle() {
+    //     const app = document.querySelector('#app');
+    //     const issue = document.querySelector('#issue');
+    //     const error = document.querySelector('#error');
+    //     const titleOutput = document.querySelector('#title');
+    //     app.addEventListener('input', function () {
+    //         titleOutput.value = `${app.value}`;
+    //     });
+    //     issue.addEventListener('input', function () {
+    //         if (app.value) {
+    //             titleOutput.value = `${app.value}`;
+    //         }
+    //         if (app.value && issue.value) {
+    //             titleOutput.value = `${app.value} - ${issue.value}`;
+    //         }
+    //     });
+    //     error.addEventListener('input', function () {
+    //         if (app.value) {
+    //             titleOutput.value = `${app.value}`;
+    //         }
+    //         if (app.value && issue.value) {
+    //             titleOutput.value = `${app.value} - ${issue.value}`;
+    //         }
+    //         if (app.value && issue.value && error.value) {
+    //             titleOutput.value = `${app.value} - ${issue.value} - ${error.value}`;
+    //         }
+    //         if (app.value && issue.value === '' && error.value) {
+    //             titleOutput.value = `${app.value} - ${error.value}`;
+    //         }
+    //     });
+    // }
+    // generateTitle();
 });
--- routes/application-routes.js
+++ routes/application-routes.js
@@ -5,4 +5,24 @@ router.get('/', function (req, res) {
     res.render('home');
 });
 
+router.post('/', function (req, res) {
+    const form = {
+        app: req.body.app,
+        issue: req.body.issue,
+        error: req.body.error,
+        date: req.body.date,
+        actions: req.body.actions,
+        outcome: req.body.outcome,
+        name: req.body.name,
+        contact: req.body.contact,
+        asset: req.body.asset,
+        location: req.body.location,
+        affected: req.body.affected,
+        submitted: true,
+    };
+
+    res.locals.form = form;
+    res.render('home');
+});
+
 module.exports = router;
